9dd0ae9c23209111dfd42ca3a12434b8302a3b03
Milly Andrew <MCMiner109@yahoo.com>
2022-12-15 18:12:30 -0800

    Create initial program.

---
+++ deckOfCards.h
@@ -0,0 +1,11 @@
+#ifndef DECKOFCARDS_H
+#define DECKOFCARDS_H
+
+class Deck
+{
+    private:
+
+    public:
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+int main () {
+    std::cout << "Hello!\nThis is a deck of card!\n";
+    return 0;
+}
